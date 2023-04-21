793997caf292f32a655fe0b3d9910e52dc9b3282
niparra21 <niparra@estudiantec.cr>
2023-04-20 21:18:47 -0600

    funciones proceso

---
+++ funciones.py
@@ -0,0 +1,20 @@
+import re
+
+#0201-304-i0506
+def decodificarPisoPasillo(pCodigo):
+    info = re.findall("-([\d]{3})-", pCodigo)[0]
+    return f"El material bibliográfico está en el piso {info[0]} en el pasillo {info[1:]}"
+
+def decodificarAreaYTipo(pCodigo):
+    listaTipos=["1","un libro","2","una revista","3","un folleto"]
+    tipo=listaTipos[listaTipos.index(pCodigo[1])+1]
+    listaArea=["1","cientifica","2","tecnologica","3","legislativa","4","medica"]
+    area=listaTipos[listaArea.index(pCodigo[3])+1]
+    return f"El material bibliográfico es {tipo} del área {area}"
+
+def decodificarDetalle(pCodigo):
+    listaLados=["i","izquierdo","d","derecho"]
+    lado=listaLados[listaLados.index(pCodigo[9])+1]
+    fila=pCodigo[10:12]
+    columna=pCodigo[12:]
+    return f"“El material bibliográfico está al lado {lado}, en la fila {fila} columna {columna}."
